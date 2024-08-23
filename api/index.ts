75590bfdc25ac9f4c0a5edcc94e2c1cf0ec8cb98
pro-opp <167337628+Pro-opp@users.noreply.github.com>
2024-08-23 17:22:48 +0500

    update backend

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,43 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import { connect } from './db/index'
-import { app } from './app';
+// import { app } from './app';
 
-dotenv.config({
-    path : './env'
-})
+import express from 'express'
+
+const app = express()
 
 
-const PORT = process.env.PORT || 8000
 
-connect()
-.then(() => {
-    app.on("error" , (err : Error) => {
-        console.log(err)
-    });
-})
-.catch((err : Error) => {
-    console.log(`Failed to connect the databse : ${err}`)
-})
+// dotenv.config({
+//     path : './env'
+// })
+
+
+const PORT = 3000
 
 app.listen(PORT , () => {
-    console.log(`Server is Listening on port ${PORT}`)
-});
-
-app.get('/' , (req : any , res : any) => {
-    res.send({status : 'connected'})
+    console.log('app is listening on port 3000')
 })
+
+
+app.get('/' , (req , res) => {
+    res.send({status : "connected"})
+})
+
+// connect()
+// .then(() => {
+//     app.on("error" , (err : Error) => {
+//         console.log(err)
+//     });
+// })
+// .catch((err : Error) => {
+//     console.log(`Failed to connect the databse : ${err}`)
+// })
+
+// app.listen(PORT , () => {
+//     console.log(`Server is Listening on port ${PORT}`)
+// });
+
+// app.get('/' , (req : any , res : any) => {
+//     res.send({status : 'connected'})
+// })
