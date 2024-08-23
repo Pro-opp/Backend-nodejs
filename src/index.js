077dc290119d47c2a00f8b48a90c81d241a022cc
pro-opp <167337628+Pro-opp@users.noreply.github.com>
2024-08-23 16:06:38 +0500

    deploy setting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import dotenv from 'dotenv'
 import { connect } from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path : './env'
 })
 
 
+const PORT = process.env.PORT || 8000
+
 connect()
+.then(() => {
+    app.listen(PORT , () => {
+        console.log(`Server is Listening on port ${PORT}`)
+    });
+    app.on("error" , (err) => {
+        console.log(err)
+    });
+    app.get('/' , (req , res) => {
+        res.send({status : 'connected'})
+    })
+})
+.catch((err) => {
+    console.log(`Failed to connect the databse : ${err}`)
+})
